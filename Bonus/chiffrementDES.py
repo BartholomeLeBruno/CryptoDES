93404e50cbf53e6e5db05aaf220ae19c5962e615
Yanis <yanisakli@yahoo.fr>
2020-01-09 23:45:48 +0100

    :fire: décalage à droite et calcule des 16 clés

--- Bonus/chiffrementDES.py
+++ Bonus/chiffrementDES.py
@@ -1,31 +1,53 @@
 from Extract_ConstantesDES import recupConstantesDES
 
+def decallageAGauche(tab):
+    result = []
+    compt = 0
+    while compt < len(tab)-1 :
+        result.insert(compt,tab[compt+1])
+        compt +=1
+    result.insert(compt,tab[0])
+    return result
 
-f = open("Clef_de_1.txt", "r")
-txt = f.read()
-f.close()
+def dictionnaireDes16Clefs() : 
+    f = open("Clef_de_1.txt", "r")
+    txt = f.read()
+    f.close()
 
-sdl = '\n'
+    sdl = '\n'
 
-X = dict()
-tab = []
-tab_G = []
-tab_D = []
+    X = dict()
+    clef = dict()
+    tab = []
+    tab_CP2=[]
+    tab_G = []
+    tab_D = []
+    tab_G_D = []
+    X=recupConstantesDES()
 
-X=recupConstantesDES()
-PI = X["PI"]
+    CP_1 = X["CP_1"]
+    CP_2 = X["CP_2"]
 
-compt = 0
+    compt = 0
 
-while compt < len(PI[0]) :
-    if (compt + 1 % 8) != 0 : 
-        tab.insert(compt,int(txt[PI[0][compt]]))
+    while compt < len(CP_1[0]) :
+        tab.insert(compt,int(txt[CP_1[0][compt]]))
         compt+=1
 
-print("taille" , len(tab))
-print("tab" , tab)
+    tab_G = tab[:28]
+    tab_D = tab[28:57]
 
-#tab_G = tab.columns[0:]
+    for i in range(0,16):
+        tab_D=decallageAGauche(tab_D)
+        tab_G=decallageAGauche(tab_G)
+        tab_G_D = tab_G + tab_D
+        compt = 0
+        while compt < len(CP_2[0]) :
+            tab_CP2.insert(compt,tab_G_D[CP_2[0][compt]])
+            compt+=1
+        clef[i]=tab_CP2
+        tab_CP2=[]
 
+    return clef
 
-
+print(dictionnaireDes16Clefs())
